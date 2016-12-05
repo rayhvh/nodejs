6a8508e5ca0cbd577ac6d5eafd177b2615e537d0
Raymond <raymondhvh@hotmail.com>
2016-12-05 11:26:59 +0100

    accept json only

--- Controllers/cdController.js
+++ Controllers/cdController.js
@@ -16,35 +16,42 @@ var cdController = function (CD) {
   var get = function (req,res) {// als de methode get gebruikt word. doe het volgende
 
       var query = {}; // lege query
+if (res.accepts('JSON')){
+    if(req.query.genre) // als de user ?genre parameter gebruikt dan add deze in query
+    {
+        query.genre = req.query.genre;
+    }
+    CD.find(query,function (err,CDs) { // zoek met mongoose idk how. uit de db via het model de cd's in array json
+        if(err)
+            res.status(500).send(err);
+        else
 
-      if(req.query.genre) // als de user ?genre parameter gebruikt dan add deze in query
-      {
-          query.genre = req.query.genre;
-      }
-      CD.find(query,function (err,CDs) { // zoek met mongoose idk how. uit de db via het model de cd's in array json
-          if(err)
-              res.status(500).send(err);
-          else
+            var returnCollection={};
+        returnCollection.items = [];
+        returnCollection._links = {};
+        returnCollection._links.self = {};
+        returnCollection._links.self.href = 'http://' + req.headers.host + '/api/CDs/';
+        returnCollection.pagination = {};
+        returnCollection.pagination.currentPage = 1;
 
-          var returnCollection={};
-              returnCollection.items = [];
-              returnCollection._links = {};
-              returnCollection._links.self = {};
-              returnCollection._links.self.href = 'http://' + req.headers.host + '/api/CDs/';
-              returnCollection.pagination = {};
-              returnCollection.pagination.currentPage = 1;
+        CDs.forEach(function (element,index,array) {
+            var newCD = element.toJSON();
+            newCD._links = {};
+            newCD._links.self={};
+            newCD._links.self.href = 'http://' + req.headers.host + '/api/CDs/' + newCD._id;
+            newCD._links.collection = {};
+            newCD._links.collection.href = 'http://' + req.headers.host + '/api/CDs/';
+            returnCollection.items.push(newCD);
+        });
+        res.json(returnCollection);//
+    });
+}
+else
+{
+    var err = "no support json";
+    res.status(400).send(err);
+}
 
-              CDs.forEach(function (element,index,array) {
-                  var newCD = element.toJSON();
-                    newCD._links = {};
-                    newCD._links.self={};
-                    newCD._links.self.href = 'http://' + req.headers.host + '/api/CDs/' + newCD._id;
-                    newCD._links.collection = {};
-                    newCD._links.collection.href = 'http://' + req.headers.host + '/api/CDs/';
-                    returnCollection.items.push(newCD);
-              });
-              res.json(returnCollection);//
-      });
   }
 
   return {
