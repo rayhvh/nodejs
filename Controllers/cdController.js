432db345d843a4fc4826230f5b410ac8d5cedf04
Raymond <raymondhvh@hotmail.com>
2016-12-05 11:30:15 +0100

    req accept.. niet res..

--- Controllers/cdController.js
+++ Controllers/cdController.js
@@ -1,63 +1,62 @@
 var cdController = function (CD) {
 
-  var post = function (req,res) {
-      var cd = new CD(req.body);
+    var post = function (req, res) {
+        var cd = new CD(req.body);
 
-      if(!req.body.title || !req.body.genre || !req.body.author ){
-          res.status(400);
-          res.send('Title and genre and author is required');
-      }
-      else {
-          cd.save();
-          res.status(201);
-          res.send(cd);
-      }
-  }
-  var get = function (req,res) {// als de methode get gebruikt word. doe het volgende
-
-      var query = {}; // lege query
-if (res.accepts('JSON')){
-    if(req.query.genre) // als de user ?genre parameter gebruikt dan add deze in query
-    {
-        query.genre = req.query.genre;
+        if (!req.body.title || !req.body.genre || !req.body.author) {
+            res.status(400);
+            res.send('Title and genre and author is required');
+        }
+        else {
+            cd.save();
+            res.status(201);
+            res.send(cd);
+        }
     }
-    CD.find(query,function (err,CDs) { // zoek met mongoose idk how. uit de db via het model de cd's in array json
-        if(err)
-            res.status(500).send(err);
-        else
+    var get = function (req, res) {// als de methode get gebruikt word. doe het volgende
 
-            var returnCollection={};
-        returnCollection.items = [];
-        returnCollection._links = {};
-        returnCollection._links.self = {};
-        returnCollection._links.self.href = 'http://' + req.headers.host + '/api/CDs/';
-        returnCollection.pagination = {};
-        returnCollection.pagination.currentPage = 1;
+        var query = {}; // lege query
+        if (req.accepts('JSON')) {
+            if (req.query.genre) // als de user ?genre parameter gebruikt dan add deze in query
+            {
+                query.genre = req.query.genre;
+            }
+            CD.find(query, function (err, CDs) { // zoek met mongoose idk how. uit de db via het model de cd's in array json
+                if (err)
+                    res.status(500).send(err);
+                else
 
-        CDs.forEach(function (element,index,array) {
-            var newCD = element.toJSON();
-            newCD._links = {};
-            newCD._links.self={};
-            newCD._links.self.href = 'http://' + req.headers.host + '/api/CDs/' + newCD._id;
-            newCD._links.collection = {};
-            newCD._links.collection.href = 'http://' + req.headers.host + '/api/CDs/';
-            returnCollection.items.push(newCD);
-        });
-        res.json(returnCollection);//
-    });
-}
-else
-{
-    var err = "no support json";
-    res.status(400).send(err);
-}
+                    var returnCollection = {};
+                returnCollection.items = [];
+                returnCollection._links = {};
+                returnCollection._links.self = {};
+                returnCollection._links.self.href = 'http://' + req.headers.host + '/api/CDs/';
+                returnCollection.pagination = {};
+                returnCollection.pagination.currentPage = 1;
 
-  }
+                CDs.forEach(function (element, index, array) {
+                    var newCD = element.toJSON();
+                    newCD._links = {};
+                    newCD._links.self = {};
+                    newCD._links.self.href = 'http://' + req.headers.host + '/api/CDs/' + newCD._id;
+                    newCD._links.collection = {};
+                    newCD._links.collection.href = 'http://' + req.headers.host + '/api/CDs/';
+                    returnCollection.items.push(newCD);
+                });
+                res.json(returnCollection);//
+            });
+        }
+        else {
+            var err = "no support json";
+            res.status(400).send(err);
+        }
 
-  return {
-      post:post,
-      get:get
-  }
+    }
+
+    return {
+        post: post,
+        get: get
+    }
 
 }
 module.exports = cdController;
